0a05cd896fbad6456b37b1487675c5d39a7a8b84
rodes nzie <rodesnzie@gmail.com>
2024-12-08 16:05:33 +0100

    add AppModal component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
    <div>
     <RouterView/>
+    <AppModal :is-open="modal.isOpen">
+
+    </AppModal>
    </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import AppModal from './components/global/AppModal.vue';
+import { useModalStore } from './stores/modal';
+
+const modal=useModalStore()
+
 </script>
 
 <style scoped>
