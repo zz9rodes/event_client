926766eaa5194f44e23263c69d8b008a57c23ac3
rodes nzie <rodesnzie@gmail.com>
2024-12-09 12:42:00 +0100

    pages in company details

---
+++ src/views/Companies/ListCompaniesPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <ListEvents />
+    <div class="inline-flex items-center justify-between w-full px-8 py-6 ">
+        <h1 class="text-xl font-bold md:text-3xl text-violet-600">Your Comapnies
+
+        </h1>
+        <div>
+            <button class="p-2 px-4 text-white rounded-md bg-violet-600" @click.prevent="isOpen=true">New</button>
+        </div>
+    </div>
+    <AppModal :is-loader="false" :is-open="isOpen" @close-modal="isOpen=false">
+        <FormCreateCompany></FormCreateCompany>
+    </AppModal>
+
+    <ListCompanies></ListCompanies>
+
+</template>
+
+<script setup>
+import ListCompanies from '@/components/Companies/ListCompanies.vue';
+import AppModal from '@/components/global/AppModal.vue';
+import FormCreateCompany from '@/components/Companies/FormCreateCompany.vue';
+import { ref } from 'vue';
+
+const isOpen=ref(false)
+</script>
+
+<style scoped></style>
---
+++ src/views/Events/ListEventPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <ListEvents />
+    <h1 class="text-sm sm:text-xl md:text-3xl">find the perfect Events  one for you!
+    </h1>
+    <CategoryBarEvent/>
+    <ListCardEvent></ListCardEvent>
+
+</template>
+
+<script setup>
+import ListEvents from '@/components/Events/SearchBarEvents.vue';
+import CategoryBarEvent from '@/components/Events/CategoryBarEvent.vue';
+import ListCardEvent from '@/components/Events/ListCardEvent.vue';
+
+</script>
+
+<style scoped></style>
