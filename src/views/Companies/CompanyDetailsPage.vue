0e0176f7a85bb36deed93d7c3d593317ee03ce28
rodes nzie <rodesnzie@gmail.com>
2024-12-09 12:43:45 +0100

    component and pages to list company what for a user is admin

---
+++ src/views/Companies/CompanyDetailsPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <CompanyHeaderDetails/>
+    <CompaniesBarMenu/>
+    <RouterView/>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import CompanyHeaderDetails from '@/components/Companies/CompanyHeaderDetails.vue';
+import CompaniesBarMenu from '@/components/Companies/CompaniesBarMenu.vue';
+
+</script>
+
+<style>
+
+</style>
