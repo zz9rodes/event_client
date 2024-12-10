a46f3aa75d75af068840f03b859807d9e76d5f88
rodes nzie <rodesnzie@gmail.com>
2024-12-10 22:35:43 +0100

    event subsribtion oage

---
+++ src/views/Events/EventPages.vue
@@ -0,0 +1,29 @@
+<template >
+    <div class="flex flex-col h-full ">
+        <nav class="bg-white shadow-sm ">
+            <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="flex items-center justify-between h-16">
+                    <div class="flex items-center">
+                        <h1 class="hidden text-xl font-bold text-violet-800 sm:block">EventHub</h1>
+                    </div>
+                    <EventBarMenu/>
+                   
+                </div>
+            </div>
+    </nav>
+
+    <RouterView class="flex-1"></RouterView>
+    </div>
+  
+
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import EventBarMenu from '@/components/Events/EventBarMenu.vue';
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Events/EventSubcribtionPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class= "">
+    <EventSubscriberList/>
+    <div class="flex items-center justify-center h-full ">
+            <div class="flex justify-between max-w-6xl grid-cols-1 gap-4 mx-auto md:grid-cols-2 lg:grid-cols-4 ">
+                <div class="flex flex-col items-center justify-center p-8 text-center rounded-lg ">
+                    <div class="relative mb-6">
+                        <svg class="w-16 h-16 text-gray-200" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M17 21V19C17 17.9391 16.5786 16.9217 15.8284 16.1716C15.0783 15.4214 14.0609 15 13 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path
+                                d="M9 11C11.2091 11 13 9.20914 13 7C13 4.79086 11.2091 3 9 3C6.79086 3 5 4.79086 5 7C5 9.20914 6.79086 11 9 11Z"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path
+                                d="M23 21V19C22.9993 18.1137 22.7044 17.2528 22.1614 16.5523C21.6184 15.8519 20.8581 15.3516 20 15.13"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path
+                                d="M16 3.13C16.8604 3.35031 17.623 3.85071 18.1676 4.55232C18.7122 5.25392 19.0078 6.11683 19.0078 7.005C19.0078 7.89318 18.7122 8.75608 18.1676 9.45769C17.623 10.1593 16.8604 10.6597 16 10.88"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        <span class="absolute -right-1 -top-1">
+                            <svg class="w-6 h-6 text-violet-600" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                                <path d="M12 8V16" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                                <path d="M8 12H16" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                            </svg>
+                        </span>
+                    </div>
+                    <h3 class="mb-2 font-medium text-gray-900">Aucun inscrit</h3>
+                    <p class="mb-6 text-sm text-gray-500">Commencez à ajouter des inscrits pour les voir apparaître ici
+                    </p>
+                  
+                </div>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script setup>
+import EventSubscriberList from '@/components/Events/EventSubscriberList.vue';
+
+</script>
+
+<style>
+
+</style>
